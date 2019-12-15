3f1404474126a62498ca9d83bdc9ba82e4f65537
Glitch (enormous-zinnia) <none>
2019-12-15 18:08:16 +0000

    👬🚘 Checkpoint ./server.js:9298160/1188

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const multer = require("multer");
 
 const app = express();
-const upload = multer();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -14,8 +14,8 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/fileanalyse", (req, res) => {
-  res.json({ greetings: "Hello, API" });
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({ greetings: req.file.size });
 });
 
 app.listen(process.env.PORT || 3000, () => {
