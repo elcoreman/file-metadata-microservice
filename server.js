d0af4998f7b0826f1c3e3dc7ad4e54585107ef89
Glitch (enormous-zinnia) <none>
2019-12-15 18:04:22 +0000

    🌻👖 Checkpoint ./package.json:9298160/1955 ./server.js:9298160/3153 ./views/index.html:9298160/2946 ./README.md:9298160/1066

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
+const upload = multer();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", (req, res) => {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Node.js listening ...");
 });
